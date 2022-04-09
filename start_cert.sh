0a1aa0d1162d3d0e80ae316e0f43fde49b6dbd9f
gri38 <onzeweb@zaclys.net>
2022-04-09 20:09:08 +0200

    Change docker-compose env variable

--- start_cert.sh
+++ start_cert.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! -d "/etc/letsencrypt/live/${server_name}" ]
+if [ ! -d "/etc/letsencrypt/live/${CERT_DOMAIN}" ]
 then
-    echo "Let's certificate ${DOMAIN}"
-    certbot certonly --webroot --webroot-path /var/www/certbot/ -d ${DOMAIN} --non-interactive --agree-tos -m ${CERT_MAIL}
+    echo "Let's certificate ${CERT_DOMAIN}"
+    certbot certonly --webroot --webroot-path /var/www/certbot/ -d ${CERT_DOMAIN} --non-interactive --agree-tos -m ${CERT_MAIL}
 fi
 
-if [ -d "/etc/letsencrypt/live/${server_name}" ]
+if [ -d "/etc/letsencrypt/live/${CERT_DOMAIN}" ]
 then
     echo "Certificated. We run for renewal..."
     trap exit TERM; while :; do certbot renew; sleep 12h; done;
